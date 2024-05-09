1098cae93d4e3ca626deb1a36290f4234f54de5f
RicardoSousaMendes <ricardomadureiramendes@gmail.com>
2024-05-09 18:40:01 +0100

    First commit & push

---
+++ JavaScript_Exercise_I/script.js
@@ -0,0 +1,36 @@
+const emprego = {
+    empresa: "threads",
+    dataInicio: "21 May"
+}
+
+let professor = {
+    name: "Vitor",
+}
+
+
+// let subtotal = 41;
+
+// const descricaoSubtotal = subtotal > 40 && "é muito caro";
+
+const Ricardo = "Designer";
+const string = "String Text"
+const boolean = true;
+
+
+Ricardo === "Designer"
+
+console.log(typeof Ricardo)
+
+const subTotal = 100;
+let total = 0;
+
+if(subTotal >= 100) {
+    total = subTotal / 1.2;
+} else if (subTotal>= 50)
+
+
+switch(subTotal){
+    case 100:
+        total = subTotal / 1.2;
+}
+
